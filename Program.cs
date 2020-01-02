5a2b9920ba34aaf08a7c6e507c1e628c2907069a
Wilson Gomes <contato@wilsonneto.com.br>
2020-01-02 13:54:00 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet_Core_Gererics_And_Hierarchy_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
